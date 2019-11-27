53f26474c195132c695921d80dd891d5f94a3f01
Pontus Salenbo <pontuss98@hotmail.com>
2019-11-27 21:57:25 +0100

    fixed m3u8 method and minor changes

--- frontend/js/svtplay.js
+++ frontend/js/svtplay.js
@@ -1,6 +1,6 @@
 const programUrl = 'https://api.svt.se/contento/graphql?ua=svtplaywebb-play-render-prod-client&operationName=ProgramsListing&variables=%7B%22legacyIds%22%3A%5B24186554%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221eeb0fb08078393c17658c1a22e7eea3fbaa34bd2667cec91bbc4db8d778580f%22%7D%7D';
 const programUrlSimple = 'https://www.svtplay.se/api/search_autocomplete_list';
-
+const programApiUrl = 'https://api.svt.se/video/';
 
 async function getURL(apiurl) {
     const data = await fetch(`http://127.0.0.1:8080/${apiurl}`, { headers: { 'x-requested-with': 'api' } });
@@ -36,13 +36,22 @@ function createSortedJson(json) {
     const sorted = json.program.sort((a, b) => parseFloat(b[1]) - parseFloat(a[1]));
     return sorted;
 }
+
+async function getM3u8Link(svtVideoId) {
+    const json = await getURL(programApiUrl + svtVideoId);
+    const m3u8 = json.videoReferences[1].url;
+    return m3u8;
+}
+
 async function anon() {
-    const jsonSimple = await getURL(programUrlSimple).then((d) => createSimpleJson(d));
-    const jsonAdvanced = await getURL(programUrl);
-    const jsonList = await createAdvancedJson(jsonSimple, jsonAdvanced);
-    console.log(jsonList);
-    const jsonListSorted = createSortedJson(jsonList);
-    console.log(jsonListSorted);
+    const jsonSimple = getURL(programUrlSimple).then((d) => createSimpleJson(d));
+    const jsonAdvanced = getURL(programUrl);
+    const promises = Promise.all([jsonSimple, jsonAdvanced]);
+    promises.then((p) => createAdvancedJson(p[0], p[1]))
+        .then((d) => createSortedJson(d))
+        .then((r) => console.log(r))
+        .catch((e) => console.error(e));
 }
 
 anon();
+getM3u8Link('eEd5x48').then(d => console.log(d));
