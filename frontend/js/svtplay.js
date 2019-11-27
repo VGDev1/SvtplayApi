617ca38ae419cb3d9de0b1874d82f5ae28bb2c5d
Victor Gunnarsson <victor@educar.se>
2019-11-27 20:22:33 +0100

    Improved Svtapi.js

--- frontend/js/svtplay.js
+++ frontend/js/svtplay.js
@@ -1,50 +1,43 @@
-const program_url =
-  "https://api.svt.se/contento/graphql?ua=svtplaywebb-play-render-prod-client&operationName=ProgramsListing&variables=%7B%22legacyIds%22%3A%5B24186554%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221eeb0fb08078393c17658c1a22e7eea3fbaa34bd2667cec91bbc4db8d778580f%22%7D%7D";
-const program_url_simple =
-  "https://www.svtplay.se/api/search_autocomplete_list";
+const programUrl = 'https://api.svt.se/contento/graphql?ua=svtplaywebb-play-render-prod-client&operationName=ProgramsListing&variables=%7B%22legacyIds%22%3A%5B24186554%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221eeb0fb08078393c17658c1a22e7eea3fbaa34bd2667cec91bbc4db8d778580f%22%7D%7D';
+const programUrlSimple = 'https://www.svtplay.se/api/search_autocomplete_list';
 
 
 async function getURL(apiurl) {
     const data = await fetch(`http://127.0.0.1:8080/${apiurl}`, { headers: { 'x-requested-with': 'api' } });
     const resp = await data.json();
     return resp;
-  }
+}
 
 function createSimpleJson(json) {
-    let data = { program: [] };
+    const data = { program: [] };
     for (let i = 0; i < json.length; i++) {
         data.program.push(
             [json[i].title,
-            json[i].popularity,
-            json[i].thumbnail]
-            ); 
+                json[i].popularity,
+                json[i].thumbnail],
+        );
     }
-  return data;
-  }
+    return data;
+}
 
-
-function createAdvancedJson(json_simple, json_advanced) {
-    let data = { program: [] }
-    for (let i = 0; i < json_advanced.data.programAtillO.flat.length; i++) {
+function createAdvancedJson(JsonSimple, JsonAdvanced) {
+    const data = { program: [] };
+    for (let i = 0; i < JsonAdvanced.data.programAtillO.flat.length; i++) {
         data.program.push(
-            [json_simple.program[i][0],
-            json_simple.program[i][1],
-            json_advanced.data.programAtillO.flat[i].i]
+            [JsonSimple.program[i][0],
+                JsonSimple.program[i][1],
+                JsonAdvanced.data.programAtillO.flat[i].i],
         );
     }
 }
 
-function createSortedJson(json, sortIndex)
-    let sortable = []
-    for (var popularity in json) {
-        
-    }
-
-
-
-function createMostPopularAdvanced(Simple, Advanced) {
-    return null;
+function createSortedJson(json, sortIndex) {
+    const sorted = json.program.sort(json.program[sortIndex]);
+    return sorted;
 }
 
-const json = getURL(program_url_simple).then(d => createSimpleJson(d));
-console.log(json);
+const jsonAdvanced = getURL(programUrl).then((d) => createAdvancedJson(d));
+const jsonSimple = getURL(programUrlSimple).then((d) => createSimpleJson(d));
+const jsonList = createAdvancedJson(jsonSimple, jsonAdvanced);
+const jsonListSorted = createSortedJson(jsonList, 3);
+console.log(jsonListSorted);
