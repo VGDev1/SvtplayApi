e64c1e335e64a32c77a67bff795628097f5959a3
Victor Gunnarsson <victor@educar.se>
2019-11-27 18:55:38 +0100

    Initial commit to svtapi

---
+++ js/svtplay.js
@@ -0,0 +1,33 @@
+
+const program_url =
+  "https://api.svt.se/contento/graphql?ua=svtplaywebb-play-render-prod-client&operationName=ProgramsListing&variables=%7B%22legacyIds%22%3A%5B24186554%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%221eeb0fb08078393c17658c1a22e7eea3fbaa34bd2667cec91bbc4db8d778580f%22%7D%7D";
+const program_url_simple =
+  "https://www.svtplay.se/api/search_autocomplete_list";
+
+
+async function getURL(apiurl) {
+    const data = await fetch(`http://127.0.0.1:8080/${apiurl}`, { headers: { 'x-requested-with': 'api' } });
+    const resp = await data.json();
+    return resp;
+  }
+
+function createSimpleJson(json) {
+  let data = { program: [] };
+  for (let i = 0; i < json.length; i++) {
+    data.program.push(
+      json[i].title,
+      json[i].popularity,
+      json[i].thumbnail
+    );
+  return data;
+  }
+}
+
+function createMostPopularAdvanced(Simple, Advanced) {
+  return null;
+}
+
+const json = getURL(program_url_simple);
+const simple_json = createSimpleJson(json);
+console.log(json);
+
