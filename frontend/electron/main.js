0527c16be2b705999eff65643be3a9a97f689bb7
Pontus Salenbo <pontuss98@hotmail.com>
2019-11-29 14:57:46 +0100

    added electron app for frontend

---
+++ frontend/electron/main.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    // New Window
+    mainWindow = new BrowserWindow({});
+    // Load HTML
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+});
